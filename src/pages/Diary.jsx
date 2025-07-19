fb8f5a2be94ec55cf0d25af9d5c18f84690c7012
jijyunnn <jijyunnn@mz.co.kr>
2025-07-19 21:38:48 +0900

    route setting2

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <>Diary</>;
+  const params = useParams();
+  console.log(params);
+  return <>{params.id}번 일기입니다.</>;
 };
 export default Diary;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  console.log(params.get("value"));
   return <>Home</>;
 };
 export default Home;
